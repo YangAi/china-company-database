85aaee5d74441123c257efa8a521c19214aeee2f
Yang Ai <yangai@YangdeMacBook-Pro.local>
2020-08-28 03:31:20 -0400

    123

--- shell/github.sh
+++ shell/github.sh
@@ -2,13 +2,14 @@ echo "Start github"
 
 git add .
 
-if [ -z "$1"]
+if [ -z "$1" ]
 then
-  echo "$1"
-  git commit -m "$1"
+  #  git commit -m "automate save"
+echo "$1"
 else
+  git commit -m "$1"
+echo "$1"
 
-  git commit -m "automate save"
 
 fi
 
